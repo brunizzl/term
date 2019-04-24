fd40d54eeab37446ea2d4ccefb71e4782109fa18
Bruno Borchardt <bruno.borchardt@tuhh.de>
2019-04-24 23:48:20 +0200

    :)

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@
 
 int main()
 {
-	test_strings();
-	test_strings_2();
-	//test_length();
+	//test_strings();
+	//test_strings_2();
+	test_length();
 	//test_function("atan(x)");
 	while (true) {
 		std::string name;
--- src/test.cpp
+++ src/test.cpp
@@ -57,18 +57,19 @@ void test_strings_2() {
 }
 
 void test_length() {
-	std::cout << "Groesse von double:         " << sizeof(double) << " bytes\n";
-	std::cout << "Groesse von long double:    " << sizeof(long double) << " bytes\n";
-	std::cout << "Groesse von list:           " << sizeof(std::list<bmath::Basic_Term>) << " bytes\n";
-	std::cout << "Groesse von Basic_Term:     " << sizeof(bmath::Basic_Term) << " bytes\n";
-	std::cout << "Groesse von Basic_Term*:    " << sizeof(bmath::Basic_Term*) << " bytes\n";
-	std::cout << "Groesse von Product:        " << sizeof(bmath::Product) << " bytes\n";
-	std::cout << "Groesse von Sum:            " << sizeof(bmath::Sum) << " bytes\n";
-	std::cout << "Groesse von Exponentiation: " << sizeof(bmath::Exponentiation) << " bytes\n";
-	std::cout << "Groesse von Variable:       " << sizeof(bmath::Variable) << " bytes\n";
-	std::cout << "Groesse von Value:          " << sizeof(bmath::Value) << " bytes\n";
-	std::cout << "Groesse von Term:           " << sizeof(bmath::Term) << " bytes\n";
-	std::cout << "Groesse von Par_Operator:   " << sizeof(bmath::Par_Operator) << " bytes\n";
+	std::cout << "Groesse von double:          " << sizeof(double) << " bytes\n";
+	std::cout << "Groesse von long double:     " << sizeof(long double) << " bytes\n";
+	std::cout << "Groesse von complex<double>: " << sizeof(std::complex<double>) << " bytes\n";
+	std::cout << "Groesse von list:            " << sizeof(std::list<bmath::Basic_Term>) << " bytes\n";
+	std::cout << "Groesse von Basic_Term:      " << sizeof(bmath::Basic_Term) << " bytes\n";
+	std::cout << "Groesse von Basic_Term*:     " << sizeof(bmath::Basic_Term*) << " bytes\n";
+	std::cout << "Groesse von Product:         " << sizeof(bmath::Product) << " bytes\n";
+	std::cout << "Groesse von Sum:             " << sizeof(bmath::Sum) << " bytes\n";
+	std::cout << "Groesse von Exponentiation:  " << sizeof(bmath::Exponentiation) << " bytes\n";
+	std::cout << "Groesse von Variable:        " << sizeof(bmath::Variable) << " bytes\n";
+	std::cout << "Groesse von Value:           " << sizeof(bmath::Value) << " bytes\n";
+	std::cout << "Groesse von Term:            " << sizeof(bmath::Term) << " bytes\n";
+	std::cout << "Groesse von Par_Operator:    " << sizeof(bmath::Par_Operator) << " bytes\n";
 	std::cout << '\n';
 }
 

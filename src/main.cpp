c3481dc0efc5e6013bf31c232ef3312be51adbc1
Bruno Borchardt <bruno.borchardt@tuhh.de>
2019-05-10 23:50:28 +0200

    Ü

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,4 @@ int main()
 	//test_length();
 	//test_function("atan(x)");
 	test_rechner();
-	int* a = new int(3);
-	int* b = new int(4);
-	int* c = new int(1);
-	int* d = new int(2);
-	int* e = new int(-5);
-	std::list<int*> l1 = { a, b, c, d, e };
-	l1.sort([](int*& a, int*& b) -> bool {return *a < *b; });
-	for (auto it : l1) {
-		std::cout << *it << ' ';
-	}
 }

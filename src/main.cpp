12bc48bb17d7f667f45914261250bedfa1b389f9
Bruno Borchardt <bruno.borchardt@tuhh.de>
2019-11-13 19:11:43 +0100

    :)

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,10 @@
 
 #include <iostream>
 #include "term.h"
-//#include "internal_functions.h"
-//#include "arguments.h"
-//#include "operations.h"
 #include "test.h"
 
 int main()
 {
-	try {
-		//test_strings();
-		//print_all_patterns();
-		//test_vergleich();
-		test_rechner();
-	}
-	catch (bmath::XTermConstructionError err) {
-		std::cout << err.what() << std::endl;
-	}
+	test_strings();
+	test_rechner();
 }
